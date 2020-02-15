b09de1c2d28c559dade2fc6c9c33d3e33b9be787
devslops <59939804+hector-develops@users.noreply.github.com>
2020-02-14 19:17:51 -0600

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:htl20219503@localhost/stockline'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:tupassword@localhost/stockline'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
